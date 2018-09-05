16a2afd45bad61c872dd983b5ed1fc323a82db2d
Ygor Batista <ygor.batista@grupocompeti.com.br>
2018-09-05 16:26:40 -0300

    cabecalho

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,11 +4,12 @@ class Home extends MY_Controller {
 
 	//Como este controller estende o MY_Controller, que é onde está a verificação de login e senha, então aqui você não precisa
 	//fazer nenhuma verifição se o usuário está logado.
-	//Lembre-se: os controllers que você deseja proteger, devem estender o MY_Controller. 
+	//Lembre-se: os controllers que você deseja proteger, devem estender o MY_Controller.
 	//O controller Login não pode estender o MY_Controller, caso contrário o código entra em loop, e também não tem sentido proteger
 	//a tela de login. :)
 	public function index()
 	{
+		$this->load->view('v_cabecalho');
 		$this->load->view('v_home');
 	}
 }
