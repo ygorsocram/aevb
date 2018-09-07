0d9dd9bb6f3dfd885212bf2e493e8149fe94c3ff
Ygor Socram <ygor.socram@gmail.com>
2018-09-07 15:04:28 -0300

    real_inicio_tela_cadastral

--- application/controllers/viuvas.php
+++ application/controllers/viuvas.php
@@ -1,11 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Viuvas extends MY_Controller {
-	public function index()
-	{
+	public function index(){
 		$variaveis['viuvas'] = $this->m_pessoa->pessoas('tabela_pessoa','1');
 		$this->load->view('cabecalhos/v_cabecalho_padrao');
 		$this->load->view('v_viuvas', $variaveis);
 		$this->load->view('rodapes/v_rodape_padrao');
+	}
+
+	public function cadastrar() {
+		$variaveis['viuvas'] = $this->m_pessoa->pessoas('info_viuva','0');
+		$this->load->view('cabecalhos/v_cabecalho_padrao');
+		$this->load->view('v_info_viuva', $variaveis);
+		$this->load->view('rodapes/v_rodape_padrao');		
 	}
 }
---
+++ application/views/v_info_viuva.php
@@ -0,0 +1,8 @@
+<li class="breadcrumb-item active">Informações Viuvas</li>
+</ol>
+
+<div class="page-content" id="info_viuva">
+  <div class="container-fluid">
+    <h1>Eu te amo</h1>
+  </div>
+</div>
