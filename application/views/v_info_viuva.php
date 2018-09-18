d9ea133c18a399cc52a2c9fb220397c811474ef2
Ygor Socram <ygor.socram@gmail.com>
2018-09-17 22:43:52 -0300

    continuando_viuva

--- application/views/v_info_viuva.php
+++ application/views/v_info_viuva.php
@@ -2,7 +2,115 @@
 </ol>
 
 <div class="page-content" id="info_viuva">
-  <div class="container-fluid">
-    <h1>Eu te amo</h1>
-  </div>
-</div>
+		<div class="container-fluid">
+			<section class="box-typical box-panel mb-4" id="form_cliente">
+        <div class="widget-container fluid-height">
+          <header class="box-typical-header">
+						<div class="tbl-cell tbl-cell-title" id="tit_iterno">
+							<strong class="titulos">
+								<i class="fa fa-female"></i>	Informações da Viuva
+							</strong>
+						</div>
+          </header>
+          <div class="box-typical-body">
+						<div class="row">
+              		<div class="col-lg-7" id="div_nome">
+              				<fieldset class="form-group">
+              					<label class="form-label" for="nome">Nome*</label>
+              					<input class="form-control proximo_campo" id="nome" name="nome" type="text" placeholder="Nome da Viuva" size="40" required>
+              				</fieldset>
+              		</div>
+                  <div class="col-lg-2" id="div_foto">
+                    <fieldset class="form-group">
+                      <label class="form-label" for="foto"></label>
+                      <input class="form-control proximo_campo" id="foto" name="foto" type="image">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-7" id="div_cep">
+                      <fieldset class="form-group">
+                        <label class="form-label" for="cep">CEP</label>
+                        <input class="form-control proximo_campo" id="cep" name="cep" type="text" size="30">
+                      </fieldset>
+                  </div>
+                  <div class="interna" id="div_endereco">
+                      <fieldset class="form-group">
+                        <label class="form-label" for="endereco">Endereço</label>
+                        <input class="form-control proximo_campo" id="endereco" name="endereco" type="text" placeholder="Endereço atual" size="80">
+                      </fieldset>
+                  </div>
+                      <div class="interna" id="div_numero">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="numero">Numero</label>
+                            <input class="form-control proximo_campo" id="numero" name="numero" type="number" size="5">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_bairro">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="bairro">Bairro</label>
+                            <input class="form-control proximo_campo" id="bairro" name="bairro" type="text" size="30">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_estado">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="estado">Estado</label>
+                            <input class="form-control proximo_campo" id="estado" name="estado" type="text" size="30">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_cidade">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="cidade">Cidade</label>
+                            <input class="form-control proximo_campo" id="cidade" name="cidade" type="text" size="30">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_telefone">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="telefone">Telefone</label>
+                            <input class="form-control proximo_campo" id="telefone" name="telefone" type="text" size="20">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_cpf">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="cpf">CPF</label>
+                            <input class="form-control proximo_campo" id="cpf" name="cpf" type="text" size="20">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_rg">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="rg">RG</label>
+                            <input class="form-control proximo_campo" id="rg" name="rg" type="text" size="20">
+                          </fieldset>
+                      </div>
+                      <div id="div_org_exp">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="org_exp">Orgão Expedidor</label>
+                            <input class="form-control proximo_campo" id="org_exp" name="org_exp" type="text" size="20">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_naturalidade">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="naturalidade">Naturalidade</label>
+                            <input class="form-control proximo_campo" id="naturalidade" name="naturalidade" type="text" size="20">
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_est_civ">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="est_civ" id="label_est_civ">Estado Civil</label>
+                            <select class="form-control proximo_campo uppercase" id="est_civ" name="est_civ" size="20">
+                            <option value="">Selecione</option>
+                            <option value="1">Viuva</option>
+                            <option value="2">Casada</option>
+                            <option value="3">Falecida</option>
+                          </fieldset>
+                      </div>
+                      <div class="interna" id="div_naturalidade">
+                          <fieldset class="form-group">
+                            <label class="form-label" for="naturalidade">Naturalidade</label>
+                            <input class="form-control proximo_campo" id="naturalidade" name="naturalidade" type="text" size="20">
+                          </fieldset>
+                    </div>
+              		</div>
+              	</div>
+							</div>
+						</section>
+          </div>
+    </div>
